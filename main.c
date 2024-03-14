8e3fc3c48d9e37308380d4468a19f0bc09ee7386
tynan Sampsel <tynansampselr@gmail.com>
2024-03-14 16:11:31 -0300

    commit 2

--- main.c
+++ main.c
@@ -20,11 +20,16 @@ int main(void)
 
     struct addrinfo hints, *servinfo, *p;
 
+	struct sockaddr_storage their_addr;
+	char buf[MAXBUFLEN];
+	socklen_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+    
     memset(&hints, 0, sizeof hints);
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_PASSIVE;
 
-    int r = getaddrinfo(NULL, MYPORT, &hints, &servinfo)
+    int r = getaddrinfo(NULL, MYPORT, &hints, &servinfo);
 
     if (r == 0)
     {
@@ -33,18 +38,18 @@ int main(void)
 
             if(s > -1){
 
-                int b = bind(sockfd, p->ai_addr, p->ai_addrlen)
+                int b = bind(sockfd, p->ai_addr, p->ai_addrlen);
 
                 break;
             }
             printf("nah\n");
         }
-
+          
 
         freeaddrinfo(servinfo);
 
         addr_len = sizeof their_addr;
-        numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)
+        numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len);
 
         printf("listener: got packet from %s\n",
                 inet_ntop(their_addr.ss_family,
