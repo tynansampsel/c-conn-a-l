1e795addf714d292641efe7088ef995692ee5ec4
tynan Sampsel <tynansampselr@gmail.com>
2024-03-14 15:44:00 -0300

    commit 1

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+void *get_in_addr(struct sockaddr *sa)
+{
+	return &(((struct sockaddr_in*)sa)->sin_addr);
+}
+
+int main(void)
+{
+
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int r = getaddrinfo(NULL, MYPORT, &hints, &servinfo)
+
+    if (r == 0)
+    {
+        for(p = servinfo; p != NULL; p = p->ai_next){
+            s = socket(p->ai_family, p->ai_socktype,p->ai_protocol);
+
+            if(s > -1){
+
+                int b = bind(sockfd, p->ai_addr, p->ai_addrlen)
+
+                break;
+            }
+            printf("nah\n");
+        }
+
+
+        freeaddrinfo(servinfo);
+
+        addr_len = sizeof their_addr;
+        numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)
+
+        printf("listener: got packet from %s\n",
+                inet_ntop(their_addr.ss_family,
+                    get_in_addr((struct sockaddr *)&their_addr),
+                    s, sizeof s));
+        printf("listener: packet is %d bytes long\n", numbytes);
+        buf[numbytes] = '\0';
+        printf("listener: packet contains \"%s\"\n", buf);
+
+        close(sockfd);
+    }
+
+    return 0;
+}
